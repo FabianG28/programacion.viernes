4b65f91944b20a0600baee0a6a22688c9551ad9c
FabianG28 <fcperezg@ufpso.edu.co>
2025-03-08 12:45:54 -0500

    Add files via upload

---
+++ quiz.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class quiz {public static void main(String[] args) {
+
+Scanner input = new Scanner(System.in);
+    System.out.print("Ingrese el primer número: ");
+    int numero1 = input.nextInt();
+    System.out.print("Ingrese el segundo número: ");
+    int numero2 = input.nextInt();
+
+    int sumaDiv1 = sumaDivisoresPropios(numero1);
+    int sumaDiv2 = sumaDivisoresPropios(numero2);
+
+    
+
+}
+    
+}
