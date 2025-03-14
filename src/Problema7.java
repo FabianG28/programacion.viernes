46884c40bd3a2549c08f53d8900559a2faeab7ab
FabianG28 <fcperezg@ufpso.edu.co>
2025-03-14 17:43:29 -0500

    Add files via upload

---
+++ src/Problema7.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Problema7 {public static void main(String[] args) {
+    
+    Scanner input = new Scanner (System.in);
+    System.out.println("ingrese el primer numero :");
+    int inicio = input.nextInt();
+    System.out.println("ingrese el segundo numero :");
+    int fin = input.nextInt(); 
+    if (inicio<1 || fin<inicio) {System.out.println(" rango no valido. ");
+    input.close();
+    return;
+    }
+    System.out.println("El primer numero es: " + inicio + " el segundo numero es:" + fin);
+
+    int contadorPrimos = 0;
+
+    for ( int i = inicio; i <= fin ; i ++) {
+
+        if (i<=1) {
+            continue;
+        }
+
+        if (i == 2) {
+            contadorPrimos+=1;
+            System.out.println(i);
+        }
+
+        if (i%2 == 1) {
+            contadorPrimos+=1;
+            System.out.println(i);
+        }
+
+    }
+System.out.println("hay " + (int) (Math.floor(contadorPrimos/2)) + " en total. ");    
+}
+
+}
